809692be1759d0b3cbc21accf2ae7bce2a860b9b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-10-06 10:03:32 -0300

    feat: Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Img from 'next/image'
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 
@@ -8,7 +9,21 @@ const Home: NextPage = () => {
       <Head>
         <title>ig.news | Home</title>
       </Head>
-      <h1 className={styles.title}>Hello</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about<br />
+            the <span className={styles.highlight}>React</span> world
+          </h1>
+          <p className={styles.subtitle}>
+            Get acess to all the publications <br />
+            <span className={`${styles.highlight} ${styles.highlightPrice}`}>for $9.90 month</span>
+          </p>
+        </section>
+        <Img src="/images/avatar.svg" alt="Girls coding" width="334" height="520" />
+      </main>
     </>
   )
 }
