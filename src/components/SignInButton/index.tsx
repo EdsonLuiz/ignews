54fed71822126fba7902a07ba535bdc931cd352b
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-10-07 10:28:00 -0300

    feat: auth with next-auth and github provider

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,20 +1,31 @@
+import {signIn, signOut, useSession} from 'next-auth/client'
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
+  const [session] = useSession()
   const isUserLoggedIn = false
 
-  const buttonMetadata = isUserLoggedIn 
-    ? {message: 'UserName', color: '#84d361'} 
+  const buttonMetadata = session 
+    ? {message: session.user?.name, color: '#84d361'} 
     : {message: 'Sign in with github', color: '#eba417'}
 
+  function handleClick() {
+    if(session) {
+      signOut()
+      return
+    }
+    signIn('github')
+  }
+  
   return (
     <button type="button"
+            onClick={handleClick}
             className={styles.signInButton}>
       <FaGithub color={buttonMetadata.color} />
       {buttonMetadata.message} 
-      {isUserLoggedIn && (
+      {session && (
         <FiX color="#737380" className={styles.closeIcon} />
       )}
     </button>
