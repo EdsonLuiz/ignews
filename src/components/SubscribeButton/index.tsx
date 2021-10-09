68cfaf0dcc992449abdd3ab0d8bcd6cd922f9e5c
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-10-09 13:37:50 -0300

    feat: redirect and create a customer on stripe.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import {signIn, useSession} from 'next-auth/client'
+import { api } from '../../services/http'
+import { getStripeJs } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 type Props = {
@@ -8,13 +10,20 @@ type Props = {
 export function SubscribeButton({priceId}: Props) {
   const [session] = useSession()
 
-  function handleClick() {
+  async function handleClick() {
     if(!session) {
       signIn('github')
       return
     }
 
-    
+    try {
+      const response = await api.post('/subscribe')
+      const {sessionId} = response.data
+      const stripe = await getStripeJs()
+      await stripe?.redirectToCheckout({sessionId})
+    } catch (error) {
+      alert(error.message)
+    }
 
   }
   return (
